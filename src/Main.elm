6b3f8a0d82f590eb1b963ec3ffc68d30967bb38d
Grant <grant.maki@integral.io>
2019-11-28 10:00:02 -0500

    added our stuff

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,13 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
+import Material.Button exposing (buttonConfig, textButton)
+import Material.TextField
+    exposing
+        ( textField
+        , textFieldConfig
+        )
+import Material.Theme as Theme
 
 
 main : Program () Model Msg
@@ -15,26 +22,76 @@ main =
 
 
 type alias Model =
-    Int
+    { name : String
+    , age : Maybe Int
+    }
 
 
 type Msg
-    = NoOp
+    = ButtonClicked
+    | GotInput TextInput String
+
+
+type TextInput
+    = NameInput
+    | AgeInput
 
 
 init : a -> ( Model, Cmd Msg )
 init _ =
-    ( 1, Cmd.none )
+    ( { name = "", age = Nothing }, Cmd.none )
 
 
 viewBody : Model -> Html Msg
 viewBody model =
-    div [] [ text "this is an elm app" ]
+    div []
+        [ div []
+            [ textField
+                { textFieldConfig
+                    | label = Just "Enter Name"
+                    , value = model.name
+                    , onInput = Just (GotInput NameInput)
+                }
+            ]
+        , div []
+            [ textField
+                { textFieldConfig
+                    | label = Just "Enter Age"
+                    , value = getYourAge model.age
+                    , onInput = Just (GotInput AgeInput)
+                }
+            ]
+        , div []
+            [ Material.Button.raisedButton
+                { buttonConfig | onClick = Just ButtonClicked }
+                "Clear"
+            ]
+        ]
+
+
+getYourAge : Maybe Int -> String
+getYourAge intMaybe =
+    case intMaybe of
+        Just int ->
+            String.fromInt int
+
+        Nothing ->
+            ""
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( 1, Cmd.none )
+    case msg of
+        ButtonClicked ->
+            ( { model | name = "", age = Nothing }, Cmd.none )
+
+        GotInput textInput string ->
+            case textInput of
+                NameInput ->
+                    ( { model | name = string }, Cmd.none )
+
+                AgeInput ->
+                    ( { model | age = String.toInt string }, Cmd.none )
 
 
 subscriptions model =
